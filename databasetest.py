a608a359b4897f7e3e5be8d9e4402f25df6b5602
tamoa1 <croometoma@gmail.com>
2025-11-07 12:06:01 +0000

    added more methods

--- databasetest.py
+++ databasetest.py
@@ -15,16 +15,37 @@ class tripDataBase:
         )
         if self.db.is_connected():
             self.cursor = self.db.cursor()
-            print("Connected to database")
+            print("connected to database")
 
     def close(self):
         if self.db.is_connected():
             self.cursor.close()
             self.db.close()
-            print("Database connection closed")
+            print("database connection closed")
 
 
 
 #student create read update delete
     def create_student(self, fName, lName, gender, dob, year, form, house, sEmail, sPhone, consent, contactID, medicalID):
         query = "INSERT INTO student (fName, lName, gender, dob, year, form, house, sEmail, sPhone, consent, contactID, medicalID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        #sql query INSERT the values held by the placeholder %s, which means only valid data can be inputted(no sql querys can be enterred)
+        values = (fName, lName, gender, dob, year, form, house, sEmail, sPhone, consent, contactID, medicalID)
+        try:
+            self.cursor().execute(query, values)
+            self.db.commit()
+            # execute the query and commit the changes to the database
+            print("student added")
+
+        except: #if there is any errors:
+            self.db.rollback() #reverse any changes to last commit
+            print("error")
+
+    def read_student(self):
+        try:
+            query = "SELECT * FROM student"
+            self.cursor().execute(query)
+            results = self.cursor().fetchall() #fetches the results of the query executed by the cursor
+        except:
+            print("error")
+
+    def update_student(self
